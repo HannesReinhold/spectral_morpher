76d68f941f5380d483dbb3e153b61a624c8d283d
Hannes Reinhold <hannes.reinhold2001@gmail.com>
2025-05-14 15:20:15 +0200

    Update Filter

--- src/CustomFilter.h
+++ src/CustomFilter.h
@@ -1,29 +1,143 @@
 #pragma once
 #include "cinder/audio/audio.h"
+#include "../../Biquad.h"
 
 using namespace ci;
 using namespace std;
 
+
+typedef std::shared_ptr<class CustomFilter>	CustomFilterRef;
+
 class CustomFilter : public audio::Node {
 public:
+	CustomFilter(const Format& format = Format()) : Node(format) {}
+	void setDrive(float drive);
 
 private:
 	void process(audio::Buffer* buffer);
+	float mDrive = 1;
 };
 
 
+
+inline void CustomFilter::setDrive(float drive)
+{
+	mDrive = drive;
+}
 
 void CustomFilter::process(audio::Buffer* buffer)
 {
 	int numChannels = buffer->getNumChannels();
 	int blockSize = buffer->getNumFrames();
 
-	for (int channel = 0; channel < numChannels; channel++) {
+	float* data = buffer->getData();
 
-		float* ch = buffer->getChannel(channel);
+	for (int sample = 0; sample < blockSize; sample++) {
+		for (int ch = 0; ch < numChannels; ch++) {
+			data[ch * blockSize + sample] = std::atan(data[ch * blockSize + sample] * mDrive);
+		}
+	}
+}
 
-		for (int sample = 0; sample < blockSize; sample++) {
-			ch[sample] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX) * 0.1f;
+
+
+
+typedef std::shared_ptr<class BiquadFilter>	BiquadFilterRef;
+
+class BiquadFilter : public audio::Node {
+public:
+	BiquadFilter(const Format& format = Format()) : Node(format) { for (int i = 0; i < 2; i++) { biquad[i] = Biquad(); } }
+	~BiquadFilter() {}
+	void setFreq(float drive);
+	void setQ(float q);
+
+private:
+	void process(audio::Buffer* buffer);
+	float mFreq = 1000;
+	Biquad biquad[2];
+};
+
+
+
+inline void BiquadFilter::setFreq(float freq)
+{
+	for (int i = 0; i < 2; i++) {
+		biquad[i].setFc(freq);
+	}
+}
+
+inline void BiquadFilter::setQ(float q)
+{
+	for (int i = 0; i < 2; i++) {
+		biquad[i].setQ(q);
+	}
+}
+
+
+
+void BiquadFilter::process(audio::Buffer* buffer)
+{
+	int numChannels = buffer->getNumChannels();
+	int blockSize = buffer->getNumFrames();
+
+	float* data = buffer->getData();
+
+	for (int sample = 0; sample < blockSize; sample++) {
+		for (int ch = 0; ch < numChannels; ch++) {
+			data[ch * blockSize + sample] = biquad[ch].process(data[ch * blockSize + sample]);
+		}
+	}
+}
+
+
+typedef std::shared_ptr<class FilterBank>	FilterBankRef;
+
+class FilterBank : public audio::Node {
+public:
+	FilterBank(const Format& format = Format()) : Node(format) { for (int i = 0; i < 2; i++) { biquad[i] = Biquad(2, 0.5f, 10, 1); } }
+	~FilterBank() {}
+	void setFreq(float drive);
+	void setQ(float q);
+
+private:
+	void process(audio::Buffer* buffer);
+	float mFreq = 1000;
+	Biquad biquad[20];
+	float frequencies[10] = { 440.0f / 44100.0f, 466.16f / 44100.0f, 493.88f / 44100.0f, 523.25f / 44100.0f,  554.37f / 44100.0f, 587.33f / 44100.0f, 622.25f / 44100.0f,659.26f / 44100.0f, 698.46f / 44100.0f, 739.99f / 44100.0f };
+};
+
+
+
+inline void FilterBank::setFreq(float freq)
+{
+	for (int i = 0; i < 10; i++) {
+		biquad[i].setFc(frequencies[i] + freq / 44100.0f);
+	}
+}
+
+inline void FilterBank::setQ(float q)
+{
+	for (int i = 0; i < 20; i++) {
+		biquad[i].setQ(q);
+	}
+}
+
+
+
+void FilterBank::process(audio::Buffer* buffer)
+{
+	int numChannels = buffer->getNumChannels();
+	int blockSize = buffer->getNumFrames();
+
+	float* data = buffer->getData();
+
+	for (int sample = 0; sample < blockSize; sample++) {
+		for (int ch = 0; ch < numChannels; ch++) {
+			float sum = 0;
+			for (int i = 0; i < 10; i++) {
+				sum += biquad[ch].process(data[ch * blockSize + sample]);
+			}
+			data[ch * blockSize + sample] = sum / 2.0f;
 		}
 	}
 }
--- src/SpectralMorpherApp.cpp
+++ src/SpectralMorpherApp.cpp
@@ -5,6 +5,7 @@
 #include ".../../../../SpectralMorpher/vc2019/MidiUtils.h"
 
 #include "cinder/audio/audio.h"
+#include ".../../../../SpectralMorpher/vc2019/CustomFilter.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -13,9 +14,9 @@ using namespace std;
 const int MAX_VOICES = 8;
 
 class SpectralMorpherApp : public App {
-  public:
+public:
 	void setup() override;
-	void mouseDown( MouseEvent event ) override;
+	void mouseDown(MouseEvent event) override;
 	void update() override;
 	void draw() override;
 
@@ -23,18 +24,27 @@ class SpectralMorpherApp : public App {
 	void keyDown(KeyEvent event) override;
 	void keyUp(KeyEvent event) override;
 
-  private:
-	  ivec2 mousePos = ivec2();
+private:
+	ivec2 mousePos = ivec2();
 
-	  Color col = Color(0,0,1);
+	Color col = Color(0, 0, 1);
 
-	  int currentPressedNotes[8] = {-1,-1,-1,-1,-1,-1,-1,-1};
+	int currentPressedNotes[8] = { -1,-1,-1,-1,-1,-1,-1,-1 };
 
-	  // audio nodes
-	  audio::GenOscNodeRef	mOsc[8];
-	  audio::GainNodeRef	mGain[8];
-	  audio::GainNodeRef		mOscAdder;
-	  audio::GainNodeRef	mMasterGain;
+	int currentOctave = 0;
+
+	// audio nodes
+	audio::GenOscNodeRef	mOsc[8];
+	audio::GainNodeRef	mGain[8];
+	audio::GainNodeRef		mOscAdder;
+	audio::GainNodeRef	mMasterGain;
+
+	audio::InputDeviceNodeRef		mInput;
+
+	CustomFilterRef mCustomFilter;
+	BiquadFilterRef mBiquad;
+	FilterBankRef mFilterBank;
+
 
 };
 
@@ -56,7 +66,16 @@ void SpectralMorpherApp::setup()
 		mOsc[i] >> mGain[i] >> mOscAdder;
 	}
 
-	mOscAdder >> mMasterGain;
+	mInput = ctx->createInputDeviceNode();
+	mInput->enable();
+	//mInput >> mOscAdder;
+
+	mCustomFilter = ctx->makeNode(new CustomFilter());
+	mBiquad = ctx->makeNode(new BiquadFilter());
+
+	mFilterBank = ctx->makeNode(new FilterBank());
+
+	mOscAdder >> mCustomFilter >> mFilterBank >> mMasterGain;
 	mMasterGain >> ctx->getOutput();
 
 	ctx->enable();
@@ -68,52 +87,63 @@ void SpectralMorpherApp::setup()
 /// </summary>
 /// <param name="event"></param>
 
-void SpectralMorpherApp::mouseDown( MouseEvent event )
+void SpectralMorpherApp::mouseDown(MouseEvent event)
 {
-	
+
 }
 
 
 void SpectralMorpherApp::mouseMove(MouseEvent event) {
 	mousePos = event.getPos();
+	//mCustomFilter->setDrive(mousePos.y*0.1f);
+	mFilterBank->setFreq(mousePos.x * 0.001f);
+	mBiquad->setQ(mousePos.y * 0.01f);
 }
 
 
 void SpectralMorpherApp::keyDown(KeyEvent event)
 {
-	int note = MidiUtils::keyToNote(event.getChar());
-	int result = MidiUtils::addNoteToPool(note, currentPressedNotes);
+	int note = MidiUtils::keyToNote(event.getChar()) + currentOctave * 12;
+	if (note >= 0) {
+		int result = MidiUtils::addNoteToPool(note, currentPressedNotes);
 
-	if (result != -1) {
-		mOsc[result]->setFreq(MidiUtils::midiToFreq(note));
-		mOsc[result]->enable();
-		mGain[result]->setValue(0.5f);
+		if (result != -1) {
+			mOsc[result]->setFreq(MidiUtils::midiToFreq(note));
+			mOsc[result]->enable();
+			mGain[result]->setValue(0.5f);
+
+
+			console() << "Added note " << (note) << " to: " << result << std::endl;
+			for (int i = 0; i < 8; i++) {
+				console() << currentPressedNotes[i] << ", ";
+			}
+			console() << std::endl;
+		}
 	}
 
-	console() << "Added note " << note << " to: " << result << std::endl;
-	for (int i = 0; i < 8; i++) {
-		console() << currentPressedNotes[i] << ", ";
-	}
-	console() << std::endl;
-	
+	if (event.getNativeKeyCode() == 38) currentOctave++;
+	if (event.getNativeKeyCode() == 40) currentOctave--;
+
 }
 
 void SpectralMorpherApp::keyUp(KeyEvent event)
 {
-	int note = MidiUtils::keyToNote(event.getChar());
-	int result = MidiUtils::removeNoteFromPool(note, currentPressedNotes);
+	int note = MidiUtils::keyToNote(event.getChar()) + currentOctave * 12;
+	if (note >= 0) {
+		int result = MidiUtils::removeNoteFromPool(note, currentPressedNotes);
+		if (result != -1) {
+			mOsc[result]->setFreq(0);
+			mOsc[result]->disable();
+			mGain[result]->setValue(0.0f);
 
-	if (result != -1) {
-		mOsc[result]->setFreq(0);
-		mOsc[result]->disable();
-		mGain[result]->setValue(0.0f);
-	}
 
-	console() << "Removed note " << note << " from: " << result << std::endl;
-	for (int i = 0; i < 8; i++) {
-		console() << currentPressedNotes[i] << ", ";
+			console() << "Removed note " << note << " from: " << result << std::endl;
+			for (int i = 0; i < 8; i++) {
+				console() << currentPressedNotes[i] << ", ";
+			}
+			console() << std::endl;
+		}
 	}
-	console() << std::endl;
 
 }
 
@@ -130,7 +160,7 @@ void SpectralMorpherApp::update()
 void SpectralMorpherApp::draw()
 {
 	col.r = (float)mousePos.x / (float)toPixels(getWindowWidth());
-	gl::clear(col); 
+	gl::clear(col);
 }
 
-CINDER_APP( SpectralMorpherApp, RendererGl )
+CINDER_APP(SpectralMorpherApp, RendererGl)
